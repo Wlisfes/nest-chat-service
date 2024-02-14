ec3b552382bafacc910d5a803628e0722fec2d56
妖雨纯 <876451336@qq.com>
2024-02-14 16:43:11 +0800

    ✨ feat(filter): 新增失败过滤器

---
+++ src/filter/http-exception.filter.ts
@@ -0,0 +1,27 @@
+import { ArgumentsHost, Catch, ExceptionFilter, HttpStatus } from '@nestjs/common'
+import * as day from 'dayjs'
+
+@Catch()
+export class HttpExceptionFilter implements ExceptionFilter {
+	catch(exception: any, host: ArgumentsHost) {
+		const ctx = host.switchToHttp()
+		const response = ctx.getResponse()
+		const request = ctx.getRequest()
+		const error = exception?.response?.hasOwnProperty('statusCode') ? exception.response ?? null : exception ?? null
+		const message = Array.isArray(exception.response?.message) ? exception.response.message[0] : exception.message
+
+		const Result = {
+			data: error,
+			message,
+			code: exception.status,
+			timestamp: day().format('YYYY-MM-DD HH:mm:ss'),
+			url: request.url,
+			method: request.method
+		}
+
+		// 设置返回的状态码、请求头、发送错误信息
+		response.status(HttpStatus.OK)
+		response.header('Content-Type', 'application/json; charset=utf-8')
+		response.send(Result)
+	}
+}
