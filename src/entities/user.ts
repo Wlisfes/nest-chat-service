7db83cfa946be2c18ad84ef3a50b898d73092d6f
妖雨纯 <876451336@qq.com>
2024-03-31 19:58:58 +0800

    ✨ feat(entities): 修改昵称字段规则

--- src/entities/user.ts
+++ src/entities/user.ts
@@ -7,34 +7,35 @@ import { CommonEntier } from '@/entities/common'
 @Entity({ name: 'user' })
 export class UserEntier extends CommonEntier {
     @ApiProperty({ description: 'UID', example: '2149446185344106496' })
-    @IsNotEmpty({ message: 'UID 必填' })
+    @IsNotEmpty({ message: 'UID必填' })
     @Column({ comment: '唯一UUID', nullable: false })
     uid: string
 
     @ApiProperty({ description: '状态: 禁用-disable、启用-enable', enum: ['disable', 'enable'], example: 'enable' })
-    @IsNotEmpty({ message: '状态 必填' })
+    @IsNotEmpty({ message: '状态必填' })
     @Column({ comment: '状态: 禁用-disable、启用-enable', default: 'enable', nullable: false })
     status: string
 
     @ApiProperty({ description: '昵称', example: '妖雨纯' })
-    @IsNotEmpty({ message: '昵称 必填' })
+    @IsNotEmpty({ message: '昵称必填' })
+    @Length(2, 32, { message: '昵称必须保持2~32位' })
     @Column({ comment: '昵称', nullable: false })
     nickname: string
 
     @ApiProperty({ description: '头像' })
-    @IsNotEmpty({ message: '头像 必填' })
+    @IsNotEmpty({ message: '头像必填' })
     @Column({ comment: '头像', nullable: false })
     avatar: string
 
     @ApiProperty({ description: '邮箱', example: 'limvcfast@gmail.com' })
-    @IsNotEmpty({ message: '邮箱 必填' })
-    @IsEmail({}, { message: '邮箱 格式错误' })
+    @IsNotEmpty({ message: '邮箱必填' })
+    @IsEmail({}, { message: '邮箱格式错误' })
     @Column({ comment: '邮箱', nullable: false })
     email: string
 
     @ApiProperty({ description: '密码', example: 'MTIzNDU2' })
-    @IsNotEmpty({ message: '密码 必填' })
-    @Length(6, 32, { message: '密码格式错误' })
+    @IsNotEmpty({ message: '密码必填' })
+    @Length(6, 32, { message: '密码必须保持6~32位' })
     @Column({
         comment: '密码',
         select: false,
