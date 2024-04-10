af980362122b5587dfcb9ebb3c5f5589eafeb269
妖雨纯 <876451336@qq.com>
2024-04-10 15:58:00 +0800

    ✨ feat(utils): 新增Buffer、Stream转化方法

--- src/utils/utils-plugin.ts
+++ src/utils/utils-plugin.ts
@@ -2,6 +2,7 @@ import { HttpException, HttpStatus } from '@nestjs/common'
 import { create } from 'svg-captcha'
 import { divineHandler } from '@/utils/utils-common'
 import * as env from '@/interface/instance.resolver'
+import * as stream from 'stream'
 
 /**条件捕获、异常抛出**/
 export async function divineCatchWherer(where: boolean, scope: env.Omix<{ message: string; status?: number; cause?: env.Omix }>) {
@@ -23,5 +24,24 @@ export async function divineGrapher(scope: env.Omix<{ width: number; height: num
         inverse: true,
         charPreset: 'ABCDEFGHJKLMNPQRSTUVWXYZ123456789',
         ...scope
+    })
+}
+
+/**Buffer转换Stream**/
+export function divineBufferToStream(buffer: Buffer): Promise<stream.PassThrough> {
+    return new Promise(resolve => {
+        const fileStream = new stream.PassThrough()
+        fileStream.end(buffer)
+        return resolve(fileStream)
+    })
+}
+
+/**Stream转换Buffer**/
+export function divineStreamToBuffer(streamFile): Promise<Buffer> {
+    return new Promise((resolve, reject) => {
+        const buffers = []
+        streamFile.on('error', reject)
+        streamFile.on('data', data => buffers.push(data))
+        streamFile.on('end', () => resolve(Buffer.concat(buffers)))
     })
 }
