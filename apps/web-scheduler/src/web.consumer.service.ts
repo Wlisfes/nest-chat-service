53d28e2714c812123b63fa34daf049d4231a2ef7
妖雨纯 <876451336@qq.com>
2024-04-16 16:22:46 +0800

    ✨ feat(rabbitmq): 绑定消息上下文

--- apps/web-scheduler/src/web.consumer.service.ts
+++ apps/web-scheduler/src/web.consumer.service.ts
@@ -1,15 +1,24 @@
-import { Inject, Injectable } from '@nestjs/common'
+import { Inject, Injectable, HttpStatus } from '@nestjs/common'
 import { RabbitSubscribe } from '@golevelup/nestjs-rabbitmq'
 import { ConsumeMessage } from 'amqplib'
 import { WINSTON_MODULE_PROVIDER } from 'nest-winston'
 import { Logger } from 'winston'
 import { CustomService } from '@/services/custom.service'
-import { divineDelay } from '@/utils/utils-common'
+import { divineDelay, divineLogger, divineIntNumber } from '@/utils/utils-common'
+import * as web from '@/config/instance.config'
 import * as env from '@/interface/instance.resolver'
 
 @Injectable()
 export class WebConsumerService {
     constructor(@Inject(WINSTON_MODULE_PROVIDER) private readonly logger: Logger, private readonly customService: CustomService) {}
+
+    /**获取自定义请求头**/
+    private async divineCustomizeHeaders(consume: ConsumeMessage) {
+        return {
+            [web.WEB_COMMON_HEADER_STARTTIME]: Date.now(),
+            [web.WEB_COMMON_HEADER_CONTEXTID]: consume.properties.messageId || (await divineIntNumber({ random: true, bit: 32 }))
+        } as never as env.Headers
+    }
 
     /**自定义消息消费者**/
     @RabbitSubscribe({
@@ -18,9 +27,22 @@ export class WebConsumerService {
         queue: 'sub-customize-transmitter'
     })
     public async SubscribeCustomizeTransmitter(data: env.Omix, consume: ConsumeMessage) {
+        const headers = await this.divineCustomizeHeaders(consume)
         try {
+            this.logger.info(
+                [WebConsumerService.name, this.SubscribeCustomizeTransmitter.name].join(':'),
+                divineLogger(headers, { message: '自定义消息消费者-开始消费', data })
+            )
             await divineDelay(1000)
-            console.log('SubscribeCustomizeTransmitter:', data)
-        } catch (e) {}
+            this.logger.info(
+                [WebConsumerService.name, this.SubscribeCustomizeTransmitter.name].join(':'),
+                divineLogger(headers, { message: '自定义消息消费者-消费结束', data })
+            )
+        } catch (e) {
+            this.logger.error(
+                [WebConsumerService.name, this.SubscribeCustomizeTransmitter.name].join(':'),
+                divineLogger(headers, { message: e.message, status: e.status ?? HttpStatus.INTERNAL_SERVER_ERROR })
+            )
+        }
     }
 }
--- src/services/rabbitmq.service.ts
+++ src/services/rabbitmq.service.ts
@@ -1,8 +1,9 @@
-import { Inject, Injectable } from '@nestjs/common'
+import { Inject, Injectable, HttpException, HttpStatus } from '@nestjs/common'
 import { AmqpConnection } from '@golevelup/nestjs-rabbitmq'
 import { WINSTON_MODULE_PROVIDER } from 'nest-winston'
 import { Logger } from 'winston'
 import { divineLogger } from '@/utils/utils-common'
+import * as web from '@/config/instance.config'
 import * as env from '@/interface/instance.resolver'
 
 @Injectable()
@@ -11,10 +12,21 @@ export class RabbitmqService {
 
     /**发送自定义消息**/
     public async despatchCustomizeTransmitter<T>(headers: env.Headers, data: env.Omix<T>) {
-        this.logger.info(
-            [RabbitmqService.name, this.despatchCustomizeTransmitter.name].join(':'),
-            divineLogger(headers, { message: '发送自定义消息', data })
-        )
-        return await this.amqpConnection.publish('web-customize-transmitter', 'sub-customize-transmitter', data)
+        try {
+            this.logger.info(
+                [RabbitmqService.name, this.despatchCustomizeTransmitter.name].join(':'),
+                divineLogger(headers, { message: '发送自定义消息', data })
+            )
+            return await this.amqpConnection.publish('web-customize-transmitter', 'sub-customize-transmitter', data, {
+                timestamp: Date.now(),
+                messageId: headers[web.WEB_COMMON_HEADER_CONTEXTID]
+            })
+        } catch (e) {
+            this.logger.error(
+                [RabbitmqService.name, this.despatchCustomizeTransmitter.name].join(':'),
+                divineLogger(headers, { message: e.message, status: e.status ?? HttpStatus.INTERNAL_SERVER_ERROR })
+            )
+            throw new HttpException(e.message, e.status ?? HttpStatus.INTERNAL_SERVER_ERROR)
+        }
     }
 }
