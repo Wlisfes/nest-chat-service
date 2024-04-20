9eeafcc393542b2b71e6598c323b890e266b377d
妖雨纯 <876451336@qq.com>
2024-04-21 00:32:23 +0800

    ✨ feat(Messager): 消息列表联查对应用户信息字段

--- src/services/messager.service.ts
+++ src/services/messager.service.ts
@@ -232,6 +232,8 @@ export class MessagerService {
             await this.httpCheckSessionBinder(userId, scope.sessionId)
             /**查询消息记录列表**/
             return await this.customService.divineBuilder(this.customService.tableMessager, async qb => {
+                /**用户信息联查**/
+                qb.leftJoinAndMapOne('t.user', entities.UserEntier, 'user', 'user.uid = t.userId')
                 /**媒体文件联查**/
                 qb.leftJoinAndMapMany('t.medias', entities.MessagerMediaEntier, 'medias', 'medias.sid = t.sid')
                 qb.leftJoinAndMapOne('medias.media', entities.MediaEntier, 'media', 'media.fileId = medias.fileId')
@@ -242,6 +244,8 @@ export class MessagerService {
                     /**消息基础字段**/
                     ...divineSelection('t', ['keyId', 'sid', 'createTime', 'updateTime', 'sessionId', 'userId']),
                     ...divineSelection('t', ['contactId', 'communitId', 'text', 'source', 'status', 'reason', 'referrer']),
+                    /**用户信息字段**/
+                    ...divineSelection('user', ['uid', 'avatar', 'nickname', 'status']),
                     /**媒体文件字段**/
                     ...divineSelection('medias', ['sid', 'fileId']),
                     ...divineSelection('media', ['source', 'fileName', 'fileSize', 'fileURL', 'width', 'height']),
