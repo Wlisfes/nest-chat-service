9922e170e4e8dc9abf8faed6ed34bf13a4c40ea5
妖雨纯 <876451336@qq.com>
2024-05-25 10:54:12 +0800

    ✨ feat(InitializeService): 新增MySQL心跳操作

--- src/services/initialize.service.ts
+++ src/services/initialize.service.ts
@@ -1,15 +1,30 @@
 import { Injectable } from '@nestjs/common'
+import { Cron, SchedulerRegistry } from '@nestjs/schedule'
 import { CustomService } from '@/services/custom.service'
 import { divineHandler } from '@/utils/utils-common'
 import * as web from '@/config/web-instance'
 
 @Injectable()
 export class InitializeService {
-    constructor(private readonly customService: CustomService) {}
+    private failure: number = 0
+    constructor(private readonly customService: CustomService, private readonly schedulerRegistry: SchedulerRegistry) {}
 
     /**模块初始化**/
     async onModuleInit() {
         await this.fetchInitializeWallpaper()
+    }
+
+    @Cron('*/45 * * * * *', { name: 'mysqlheartbeat' })
+    private async fetchInitializeHeartbeat() {
+        const job = this.schedulerRegistry.getCronJob('mysqlheartbeat')
+        try {
+            await this.customService.tableWallpaper.count()
+        } catch (e) {
+            return divineHandler(this.failure > 3, {
+                failure: () => this.failure++,
+                handler: () => job.stop()
+            })
+        }
     }
 
     /**背景颜色列表初始化**/
